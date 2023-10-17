e0f69f377471e7c209c9e656f3699229fb866397
Mykhailo Pokras <mspokras@gmail.com>
2023-10-17 15:35:21 +0300

    Updated prettier files added

---
+++ dist/home.5f081e1336733a013453.hot-update.js
@@ -0,0 +1,11 @@
+"use strict";
+self["webpackHotUpdatefls_start"]("home",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "17aff4176aad1b3df78a"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaG9tZS41ZjA4MWUxMzM2NzMzYTAxMzQ1My5ob3QtdXBkYXRlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O1VBQUEscUNBQXFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vZmxzLXN0YXJ0L3dlYnBhY2svcnVudGltZS9nZXRGdWxsSGFzaCJdLCJzb3VyY2VzQ29udGVudCI6WyJfX3dlYnBhY2tfcmVxdWlyZV9fLmggPSBmdW5jdGlvbigpIHsgcmV0dXJuIFwiMTdhZmY0MTc2YWFkMWIzZGY3OGFcIjsgfSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
